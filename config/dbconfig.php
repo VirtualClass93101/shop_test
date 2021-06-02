a1e0b369ed0a9fd229820df5b26b9b7b049f775b
Randy Huang <61420509+VirtualClass93101@users.noreply.github.com>
2021-06-02 11:43:29 +0800

    Update dbconfig.php

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 
-	$db_hostname="localhost"; $db_username="your username"; $db_password="your password"; $db_database="db_shop_test";
+	$db_hostname="localhost"; $db_username="your username"; $db_password="your password"; $db_database="db_shop";
 	if(!($dblink=mysqli_connect($db_hostname,$db_username,$db_password))){ echo("mysql connect false"); exit(); }
 	mysqli_query("SET NAMES 'UTF8'");
 	if (!mysqli_select_db($dblink,$db_database)){ echo("select database false"); exit(); }
