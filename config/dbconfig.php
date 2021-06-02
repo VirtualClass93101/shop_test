82f6b49b4dd2b1e3a2bdc4ca5c562311d6a0bda3
Randy Huang <61420509+VirtualClass93101@users.noreply.github.com>
2021-06-02 11:22:22 +0800

    修改提示字眼

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,14 +1,7 @@
 <?php
 
-	$db_hostname="localhost"; $db_username="root"; $db_password="1234"; $db_database="db_shop_test";
+	$db_hostname="localhost"; $db_username="your username"; $db_password="your password"; $db_database="db_shop_test";
 	if(!($dblink=mysqli_connect($db_hostname,$db_username,$db_password))){ echo("mysql connect false"); exit(); }
 	mysqli_query("SET NAMES 'UTF8'");
 	if (!mysqli_select_db($dblink,$db_database)){ echo("select database false"); exit(); }
-	
-
-	// function myquery($sql=null){
-	// 	global $dblink;
-	// 	$res = mysqli_query($dblink,$sql);
-	// 	return $res;
-	// }
 ?>
