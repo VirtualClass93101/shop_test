fc561fbb6265d54339cc17ca3536a154db41d737
Randy Huang <61420509+VirtualClass93101@users.noreply.github.com>
2021-06-02 10:56:02 +0800

    假設有後台的情況
    /manage/index.php 後臺設定(設定商品數量,上架下架,優惠券套用)
    /tmp/commodity_item.php 後臺設定完後，將商品的編號,品名,金額 記錄下來的tmp檔案
    /tmp/commodity_item.php 後臺設定完後，將商品類型的編號,項目 記錄下來的tmp檔案

---
+++ tmp/commodity_item.php
@@ -0,0 +1,23 @@
+<?php
+	$commodityItem = array();
+	$commodityItemPrice = array();
+	$commodityItemShip = array();
+
+	$commodityItem[1] = "運動飲料";
+	$commodityItem[2] = "爆米花";
+	$commodityItem[3] = "鍋具";
+	$commodityItem[4] = "魚";
+	$commodityItem[5] = "飛機";
+
+	$commodityItemPrice[1] = 20;
+	$commodityItemPrice[2] = 30;
+	$commodityItemPrice[3] = 999;
+	$commodityItemPrice[4] = 399;
+	$commodityItemPrice[5] = 9999;
+
+	$commodityItemShip[1] = 5;
+	$commodityItemShip[2] = 2;
+	$commodityItemShip[3] = 100;
+	$commodityItemShip[4] = 50;
+	$commodityItemShip[5] = 0;
+?>
---
+++ tmp/commodity_type.php
@@ -0,0 +1,9 @@
+<?php
+	$commodityType = array();
+	$commodityType[0] = "全部";
+	$commodityType[1] = "居家生活雜貨";
+	$commodityType[2] = "收納用品";
+	$commodityType[3] = "家事清潔";
+	$commodityType[4] = "廚具用品";
+	$commodityType[5] = "日用";
+?>
